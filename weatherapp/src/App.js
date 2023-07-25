10541d0b64417f642339436d1a66c275a9e0582e
Eman <emanamberzindani@gmail.com>
2023-07-25 12:21:11 -0500

    fixed api issues-now working

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 const api = {
   key: "7907a22002aa76a693df37bf684e6845",
-  base: "https://api.openweathermap.org/data/2.5"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
